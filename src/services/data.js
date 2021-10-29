86d25674115b49b0bd48f22d3691d68b063e4436
FelipeNevess <felipe012neves@gmail.com>
2021-10-28 21:47:47 -0300

    estrutura de video pronta

---
+++ src/services/data.js
@@ -0,0 +1,48 @@
+import img_teste from '../images/img_teste.jpg';
+
+const data = {
+  video_1: {
+    id: 1,
+    url: 'https://player.vimeo.com/video/640156298?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0h=e2037395a5&title=0&byline=0&portrait=0&speed=0&badge=0&autopause=0&player_id=0&app_id=58479/embed',
+    poster: img_teste,
+    description: '',
+    name: ''
+  },
+  video_2: {
+    id: 2,
+    url: 'https://player.vimeo.com/video/120680495?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0',
+    poster: img_teste,
+    description: '',
+    name: ''
+  },
+  video_3: {
+    id: 3,
+    url: 'https://player.vimeo.com/video/120680495?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0',
+    poster: img_teste,
+    description: '',
+    name: ''
+  },
+  video_4: {
+    id: 4,
+    url: 'https://player.vimeo.com/video/120680495?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0',
+    poster: img_teste,
+    description: '',
+    name: ''
+  },
+  video_5: {
+    id: 5,
+    url: 'https://player.vimeo.com/video/120680495?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0',
+    poster: img_teste,
+    description: '',
+    name: ''
+  },
+  video_6: {
+    id: 6,
+    url: 'https://player.vimeo.com/video/120680495?autoplay=1&loop=1&color=ffffff&title=0&byline=0&portrait=0',
+    poster: img_teste,
+    description: '',
+    name: ''
+  }
+}
+
+export default data;
