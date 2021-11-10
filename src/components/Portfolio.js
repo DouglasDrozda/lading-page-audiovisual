04e21fe89436aa725276d803e4b3088e549a7c5a
DouglasDrozda <lowdouglas9712@gmail.com>
2021-11-09 22:04:10 -0300

    estrutura pronta dos vídeos

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-
 import '../styles/Portfolio.css';
-
+import { Modal } from 'react-bootstrap';
 import data from '../services/data';
 import play from '../icons/play.png';
 
 function Portfolio() {
-  const [classPlay, setClassPlay] = useState('play-off');
   const [urlVideo, setUrlVideo] = useState('');
-  
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(!show);
+
   const handleClick = (url) => {
     setUrlVideo(url);
-    setClassPlay('play-on');
   }
 
   return (
@@ -21,29 +21,39 @@ function Portfolio() {
           <h1>PORTFÓLIO</h1>
         </div>
         <div className="video-maker">
-          <div className="row1">
-            {
-              Object.values(data).map((video) => {
-                return (
-                  <div className="description-video" key={ video.id } onClick={ () => handleClick(video.url) }>
-                    <video poster={ video.poster } className="video_play">
-                      <source src={ video.url } type="video/mp4" />
-                    </video>
-                    <div className="on_play">
-                      <img src={ play } alt="play" />
+          <div className="row1" onClick={handleShow}>
+            <Modal
+              show={show}
+              backdrop="static"
+              keyboard={false}
+            >
+              <Modal.Header closeButton>
+                <iframe
+                  src={urlVideo}
+                  title="video"
+                  allow="autoplay; fullscreen; picture-in-picture"
+                  allowfullscreen
+                  frameborder="0"
+                  className='play-on'
+                />
+              </Modal.Header>
+            </Modal>
+            <div className="description-video-container">
+              {
+                Object.values(data).map((video) => {
+                  return (
+                    <div className="description-video" key={video.id} onClick={() => handleClick(video.url)}>
+                      <div poster={video.poster} className="video_play">
+                        <img src={video.url} alt={video.name} />
+                      </div>
+                      <div className="on_play">
+                        <img src={play} alt="play" />
+                      </div>
                     </div>
-                  </div>
-                )
-              })
-            }
-            <iframe
-              src={ urlVideo }
-              title="video"
-              allow="autoplay; fullscreen; picture-in-picture"
-              allowfullscreen
-              frameborder="0"
-              className={ classPlay }
-            />
+                  )
+                })
+              }
+            </div>
           </div>
         </div>
       </div>
@@ -52,3 +62,4 @@ function Portfolio() {
 }
 
 export default Portfolio;
+
